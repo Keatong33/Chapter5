7f51dffb439d95cc0474c4469e5e6e85dedd5504
Greenwalt <KG155781@davenportschools.org>
2019-04-29 12:11:14 -0500

    update

---
+++ src/AssignVolunteer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AssignVolunteer {
+    public static void main(String[] args){
+        int donationType;
+        String volunteer;
+        final int CLOTHING_CODE = 1;
+        final int OTHER_CODE = 2;
+        final String CLOTHING_PRICER = "Regina";
+        final String OTHER_PRICER = "Marco"; 
+    }
+
+}
